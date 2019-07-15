b56e0839c8f4656556f9660f09edf990de29e487
Naimaoye <naimatoyewale@gmail.com>
2019-07-15 23:23:31 +0100

    (feature):add user can delete bookings

--- API/routes/bookings.js
+++ API/routes/bookings.js
@@ -7,6 +7,12 @@ const router = express.Router();
 // book a seat in a trip
 router.post('/bookings', Authentication.verifyToken, Bookings.bookAtrip);
 
+// get all bookings
+router.get('/bookings', Authentication.verifyToken, Bookings.getAllBookings);
+
+// user can delete their bookings
+router.delete('/bookings/:booking_id', Authentication.verifyToken, Bookings.deleteBooking);
+
 
 
 export default router;
