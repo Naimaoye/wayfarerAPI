3c323d71b72a6eb32a4edcb43b2b8734d5a52ca6
Naimaoye <naimatoyewale@gmail.com>
2019-07-15 15:38:40 +0100

    adjust seed data

--- API/migrations/seedData.js
+++ API/migrations/seedData.js
@@ -22,4 +22,49 @@ const users = [
 }
 ];
 
-export default users;
+const bus = [
+    {
+       number_plate: 'ABJ32-456',
+       manufacturer: 'Toyota',
+       model: 'Hiace',
+       year: '2018',
+       capacity: 36,
+       created_on: new Date()
+    },
+    {
+        number_plate: 'LAG25-6336',
+        manufacturer: 'Suzuki',
+        model: 'Fortnight',
+        year: '2016',
+        capacity: 40,
+        created_on: new Date()
+    }
+];
+
+const trip = [
+    {
+        bus_id: 1,
+        created_on: new Date(),
+        origin : 'oyo', 
+        destination: 'katsina', 
+        trip_date: new Date("July 20, 2019 11:13:00"), 
+        fare: 2010.033, 
+        status: 'active',
+        modified_on: new Date()
+    },
+    {
+        bus_id: 2,
+        created_on: new Date(),
+        origin : 'lagos', 
+        destination: 'abuja', 
+        trip_date: new Date("July 28, 2019 11:13:00"),
+        fare: 200.033, 
+        status: 'active',
+        modified_on: new Date()
+       }
+ ]
+export {
+    users,
+    bus,
+    trip
+};
