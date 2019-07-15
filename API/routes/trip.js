5539fd8cb8b5eb6353f0fafec52fd162e8be1d12
Naimaoye <naimatoyewale@gmail.com>
2019-07-15 16:19:48 +0100

    adjust trip route

--- API/routes/trip.js
+++ API/routes/trip.js
@@ -11,6 +11,14 @@ router.post('/trips', Authentication.verifyToken, Trip.createAtrip);
 router.post('/trips/bus', Authentication.verifyToken, Trip.addBusForTrip);
 
 // user can filter trips using destination or origin, while admin can view all trip
-router.get('/trips', Trip.getTripByDestOrOrigin, Trip.getAllTrips);
+router.get('/trips', Authentication.verifyToken,
+  Trip.getTripByDestOrOrigin,
+  Trip.getAllTrips);
 
+// // admin and user can get all trips
+// router.get('/trips', Authentication.verifyToken, Trip.getAllTrips);
+/*
+// admin can cancel a trip
+router.patch('/trips/:trip_id', Authentication.verifyToken, Trip.cancelATrip);
+*/
 export default router;
