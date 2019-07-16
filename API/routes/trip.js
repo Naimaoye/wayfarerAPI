9c66cca06f65de0ad143373113458a6fb67b716d
Naimaoye <naimatoyewale@gmail.com>
2019-07-16 23:01:52 +0100

    (bug):fix routes error

--- API/routes/trip.js
+++ API/routes/trip.js
@@ -5,13 +5,13 @@ import Authentication from '../middleware/Authentication';
 const router = express.Router();
 
 // admin can create trip
-router.post('/trips', Authentication.verifyToken, Trip.createAtrip);
+router.post('trips', Authentication.verifyToken, Trip.createAtrip);
 
 // admin can add bus to database
-router.post('/trips/bus', Authentication.verifyToken, Trip.addBusForTrip);
+router.post('trips/bus', Authentication.verifyToken, Trip.addBusForTrip);
 
 // user can filter trips using destination or origin, while admin can view all trip
-router.get('/trips', Authentication.verifyToken,
+router.get('trips', Authentication.verifyToken,
   Trip.getTripByDestOrOrigin,
 Trip.getAllTrips);
 
